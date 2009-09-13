c52124824e19d14081527b906f1e448fb841f7cd
Nick Carter <thynctank@thynctank.com>
2009-09-13 06:24:51 -0400

    transfers working w/ transactions properly

--- account.js
+++ account.js
@@ -50,10 +50,13 @@ Account.prototype = {
           callback(self);
       });
     };
+    if(this.entries.length === 0)
+      this.loadEntries();
+    // if still 0, must be new acct
     if(this.entries.length === 0) {
-      if(this.entries.length === 0 && this.balance > 0)
+      if(this.balance > 0)
         this.credit({subject: "Current Balance", category: "Starting balance", cleared: 1, amount: this.balance, calledFromSave: true}, updateBalance);
-      else if(this.entries.length === 0 && this.balance < 0)
+      else if(this.balance < 0)
         this.debit({subject: "Current Balance", category: "Starting balance", cleared: 1, amount: Math.abs(this.balance), calledFromSave: true}, updateBalance);
       else
         updateBalance();
@@ -93,7 +96,7 @@ Account.prototype = {
     this.writeEntry(options, success);
   },
   // takes regular options obj but also takes transferAccountName
-  transfer: function(options) {
+  transfer: function(options, callback) {
     // setup a debit on this acct and a credit on the other acct
     if(!options || !options.transferAccountName)
       throw("Error. Minimum data for transfer (transferAccountName) not present");
@@ -102,29 +105,40 @@ Account.prototype = {
       if(!thatAccount)
         throw("Error. Transfer account does not exist");
       else {
-        options.subject = options.subject ? options.subject : "Transfer: " + this.name + " to " + thatAccount.name;
-        var theseOptions = options;
-        var thoseOptions = options;
+        options.category = options.subject = options.subject ? options.subject : "Transfer: " + this.name + " to " + thatAccount.name;
+        var theseOptions = this.getEntryOptions(options);
+        var thoseOptions = this.getEntryOptions(options);
+        theseOptions.type = "debit";
         theseOptions.transfer_account_id = thatAccount.id;
+        thoseOptions.type = "credit";
+        thoseOptions.account_id = thatAccount.id;
         thoseOptions.transfer_account_id = this.id;
         
+        var storage = this.checkbook.storage;
         var self = this;
         
-        self.debit(theseOptions, function(thisInsertId) {
-          // set to ID obtained from first writeEntry
-          thoseOptions.transfer_entry_id = thisInsertId;
-          // get second ID for third db hit
-          thatAccount.credit(thoseOptions, function(thatInsertId) {
-            theseOptions.transfer_entry_id = thatInsertId;
-            theseOptions.id = thisInsertId;
-            theseOptions.type = "debit";
-            self.writeEntry(theseOptions);
-          }, function() {
-            // rollback if second transaction failed
-            self.checkbook.storage.erase("entries", {id: thisInsertId}, function() {
-              self.save();
-            });
-          });
+        storage.transact(function(tx) {
+          // write debit portion
+          storage.write("entries", theseOptions, function(thisInsertId) {
+            thoseOptions.transfer_entry_id = thisInsertId;
+            // write credit portion w/ transfer_account_id and transfer_entry_id set appropriately
+            storage.write("entries", thoseOptions, function(thatInsertId) {
+              // update debit portion w/ second transfer_entry_id
+              theseOptions.id = thisInsertId;
+              theseOptions.transfer_entry_id = thatInsertId;
+              storage.write("entries", theseOptions, null, null, tx);
+            }, null, tx);
+          }, null, tx);
+        }, function() {
+          // push entry on this acct, save
+          self.entries.push(theseOptions);
+          self.save();
+          // push entry on that acct, save
+          thatAccount.entries.push(thoseOptions);
+          thatAccount.save();
+          
+          if(callback)
+            callback();
         });
       }
     }
@@ -132,47 +146,44 @@ Account.prototype = {
   eraseEntry: function(index, callback) {
     var self = this;
     var entry = this.entries[index];
-    var afterErase = function() {
-      self.entries.splice(index, 1);
-      self.balance = self.getBalance();
-      self.save(callback);
-    };
     
-    this.checkbook.storage.erase("entries", {id: entry.id}, afterErase);
-    // erase both sides of a transfer
-    if(entry.transfer_entry_id) {
-      this.checkbook.storage.erase("entries", {id: entry.transfer_entry_id});
-      var thatAccount = this.checkbook.getAccountById(entry.transfer_account_id);
-      // if entry is debit, other entry must be credit, so subtract balance to reverse
-      if(entry.type == "debit")
-        thatAccount.balance -= entry.amount;
-      else
-        thatAccount.balance += entry.amount;
-
-      thatAccount.save();
-    }
+    this.checkbook.storage.erase("entries", {id: entry.id}, function() {
+      self.entries.splice(index, 1);
+      self.save(callback);
+      // erase both sides of a transfer
+      if(entry.transfer_entry_id) {
+        self.checkbook.storage.erase("entries", {id: entry.transfer_entry_id}, function() {
+          var thatAccount = self.checkbook.getAccountById(entry.transfer_account_id);
+          thatAccount.save();
+        });
+      }
+    });
   },
   // utility functions
   // transact should be able to rollback if something goes wrong, save if all works
+  getEntryOptions: function(options) {
+    options = {
+      account_id: this.id,
+      type: options.type,
+      category: options.category,
+      subject: options.subject || options.category,
+      amount: options.amount,
+      id: options.id || null,
+      date: options.date || (new Date().getTime()),
+      memo: options.memo || null,
+      transfer_account_id: options.transfer_account_id || null,
+      transfer_entry_id: options.transfer_entry_id || null,
+      cleared: options.cleared || 0,
+      check_number: options.check_number || null
+    };
+    return options;
+  },
   writeEntry: function(options, callback) {
+    // require minimum options of type, category, amount
     if(!options || !options.category || !options.type || !options.amount)
-      throw("Error. Minimum data for entry (subject, amount) not present");
+      throw("Error. Minimum data for entry not present");
     else {
-      // require minimum options of type, subject, amount
-      options = {
-        account_id: this.id,
-        type: options.type,
-        category: options.category,
-        subject: options.subject || options.category,
-        amount: options.amount,
-        id: options.id || null,
-        date: options.date || (new Date().getTime()),
-        memo: options.memo || null,
-        transfer_account_id: options.transfer_account_id || null,
-        transfer_entry_id: options.transfer_entry_id || null,
-        cleared: options.cleared || 0,
-        check_number: options.check_number || null
-      };
+      options = this.getEntryOptions(options);
 
       var self = this;
       var storage = self.checkbook.storage;
